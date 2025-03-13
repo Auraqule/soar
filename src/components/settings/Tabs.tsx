f86ae6aca78b1e33ea975cb7609f4dc88b55b768
Christian Peters <auraqule@gmail.com>
2025-03-13 18:11:31 +0100

    upd: added transition to tab switch

--- src/components/settings/Tabs.tsx
+++ src/components/settings/Tabs.tsx
@@ -8,7 +8,7 @@ const Tabs = ({ activeTab, setActiveTab }: TProps) => {
     <div className="border-b border-[#F4F5F7]">
       <nav className="flex">
         <button
-          className={`px-4 py-4 text-sm font-medium transition-all duration-150 ease-linear ${
+          className={`px-4 py-4 text-sm font-medium transition-all duration-300 ease-linear ${
             activeTab === "edit-profile"
               ? "text-tab-active-text border-b-2 border-tab-active-text hover:!border-[#232323]"
               : "text-tab-inactive-text hover:text-tab-active-text"
@@ -18,7 +18,7 @@ const Tabs = ({ activeTab, setActiveTab }: TProps) => {
           Edit Profile
         </button>
         <button
-          className={`px-4 py-4 text-sm font-medium ${
+          className={`px-4 py-4 text-sm font-medium transition-all duration-300 ease-linear ${
             activeTab === "preferences"
               ? "text-tab-active-text border-b-2 border-tab-active-text hover:!border-[#232323]"
               : "text-tab-inactive-text hover:text-tab-active-text"
@@ -28,7 +28,7 @@ const Tabs = ({ activeTab, setActiveTab }: TProps) => {
           Preferences
         </button>
         <button
-          className={`px-4 py-4 text-sm font-medium ${
+          className={`px-4 py-4 text-sm font-medium transition-all duration-300 ease-linear ${
             activeTab === "security"
               ? "text-tab-active-text border-b-2 border-tab-active-text hover:!border-[#232323]"
               : "text-tab-inactive-text hover:text-tab-active-text"
